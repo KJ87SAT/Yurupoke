a10738a8d87818f3994150eeecf140456aa0a32c
KJ87SAT <kenjikubo0324@gmail.com>
2024-12-31 16:23:56 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const joinButton = document.querySelector('.discord-button');
+    
+    joinButton.addEventListener('click', () => {
+        alert("Discordサーバーに参加してくれてありがとう！");
+    });
+});
